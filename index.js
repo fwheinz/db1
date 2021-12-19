afe537781ed55d58120971d1cd0312d71ef8b4ad
Florian Heinz <florian.heinz@oth-regensburg.de>
2021-12-19 20:44:00 +0000

    Changed stuff

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express()
 const port = 80
 // Serve static files from folder 'static'
 app.use(express.static('static'))
+// Decode form submitted values to req.body.<key>
 app.use(require('body-parser').urlencoded({ extended: false }))
+// Permit session persistent data in req.session.<key>
+const session = require('express-session')
+app.use(session({ secret: 'masterblaster', cookie: { maxAge: 86400000 }}))
 
 // Setup handlebars template engine
 const { engine } = require('express-handlebars')
@@ -15,12 +19,21 @@ app.engine('hbs', engine({
   extname: 'hbs' }))
 
 // Setup database connection
-const connuri = process.env["DBCONN"];
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+const connuri = process.env["DBCONN"]
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
 const { Pool, Client } = require('pg')
 const connectionString = connuri + '?sslmode=require'
 const client = new Client ({ connectionString })
 client.connect()
+
+app.use(function (req, res, next) {
+  console.log(req);
+  if (!req.session.user && req.path != "/login") {
+    res.render('login');
+  } else {
+    next();
+  }
+});
 
 /**********************************************************************/
 /*                                                                    */
@@ -28,32 +41,75 @@ client.connect()
 /*                                                                    */
 /**********************************************************************/ 
 
+// Log into the webshop. Check username and password and set the
+// session variable "user" and "name" if authentication succeeds
+app.post('/login', (req, res) => {
+  client.query({
+    text: 'SELECT * FROM customers WHERE cno=$1 AND password=$2',
+    values: [req.body.username, req.body.password]},
+    (err, data) => {
+      if (!err && data.rowCount == 1) {
+        req.session.user = data.rows[0].cno
+        req.session.name = data.rows[0].name
+        res.redirect(301, '/')
+      } else {
+        res.render('login', {err: 'Authentication failed'})
+      }
+    })
+})
+ 
+
+// Log out from the webshop by unsetting the session variables
+app.get('/logout', (req, res) => {
+  req.session.user = undefined;
+  req.session.name = undefined;
+  res.render('login')
+})
 
 // Render the page views/index.hbs
 app.get('/', (req, res) => {
-  res.render("index");
+  res.render('index', {name: req.session.name})
+})
+
+// Render the page views/admin.hbs
+app.get('/admin', (req, res) => {
+  res.render('admin', {name: "Aperture"})
 })
 
 // Render a list of all customers
 app.get('/customers', (req, res) => {
-  var q = 'SELECT cno, name, count(id) as nrorders FROM customers '+
-          'LEFT JOIN orders USING (cno) GROUP BY cno'
-  doQuery(res, q, 'customers')
+  let q1 = 'SELECT cno, name, count(id) as nrorders FROM customers '+
+          'LEFT JOIN orders USING (cno) GROUP BY cno,name ORDER BY name'
+  let q2 = 'SELECT count(*) AS numbercustomers FROM customers'
+  doQuery(res, 'customers', null, q1, q2)
 })
 
 // Render customer search result
-app.get('/customersearch', (req, res) => {
-  var q = 'SELECT cno, name FROM customers '+
-          'WHERE name like \'%'+req.query.search+'%\''
-  doQuery(res, q, 'customers');
+app.post('/customersearch', (req, res) => {
+  let search = req.body.search
+  let q = 'SELECT cno, name FROM customers '+
+          `WHERE name like '%${search}%'`
+  doQuery(res, 'customers', null, q);
 })
 
 // Show the items of the webshop at a given offset
+// This query uses a prepared statement
 app.get('/shop/:offset', (req, res) => {
-  var offset = Number(req.params.offset || 0);
-  var q = 'SELECT * FROM products LIMIT 4 OFFSET '+offset;
-  doQuery(res, q, 'shop', { next: offset+4, 
-                            prev: offset-4})
+  let offset = Number(req.params.offset)
+  let q = { text: 'SELECT * FROM products LIMIT 4 OFFSET $1',
+            values: [offset] }
+  doQuery(res, 'shop', { next: offset+4, 
+                         prev: offset-4}, q)
+})
+
+// Show the items of the webshop at a given offset
+// This query uses a prepared statement
+app.post('/shopsearch', (req, res) => {
+  let offset = Number(req.params.offset)
+  let pattern = req.body.pattern
+  let q = `SELECT name,price FROM products WHERE name LIKE '%${pattern}%'`
+  doQuery(res, 'shop', { next: 0, 
+                         prev: 0}, q)
 })
 
 
@@ -63,21 +119,28 @@ app.get('/shop/:offset', (req, res) => {
 // query:    the database query to perform 
 // template: the template file to render (in directory "views")
 // params:   further parameters to pass to the template
-function doQuery (res, query, template, params = {}) {
-  client.query(query, 
-    (err, data) => {
-      if (err) {
-        err.query = query;
-        err.msg = String(err);
-        res.render('error', err);
-      } else {
-        for (var key in params) {
-          data[key] = params[key];
+function doQuery (res, template, params, ...queries) {
+  _doQuery(res, template, params || {}, 1, queries)
+}
+function _doQuery (res, template, params, depth, queries) {
+  if (queries.length > 0) {
+    let query = queries.shift();
+    client.query(query, 
+      (err, data) => {
+        if (err) {
+          err.query = JSON.stringify(query);
+          err.msg = err.message
+          res.render('error', err)
+        } else {
+          console.log(JSON.stringify(data))
+          params['result'+depth] = data
+          _doQuery(res, template, params, depth+1, queries)
         }
-        res.render(template, data)
       }
-    }
-  )
+    )
+  } else {
+    return res.render(template, params)
+  }
 }
 
 // Open listen port and serve requests
